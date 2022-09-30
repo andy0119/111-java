ccbe3a264b0ba7676d211e55ce9ff6f4fd4e656f
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-09-30 09:04:40 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String[] args){
         System.out.println(args[0]);
-        printStr("Chris!");
+        printStr("andy");
     }
     
     public static void printStr(String str) {
