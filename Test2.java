55cdc4fc87ee5550e29b268a65fafbf882fe0010
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-09-30 11:45:27 +0800

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,19 @@
+import java util Scanner;
+
+public class Test2 {
+    public static void main(String[] args){
+        int num1;
+        int num2;
+        int sum;
+        
+        scanner input = new Scanner(system.in);
+        system out println("enter first integer:")
+        num1= input nextint();
+        
+        system out println("enter second integer:")
+        num2= input nextint();
+        
+        sum = num1 = num2;
+        system out println(The sum is' +sum) ;
+        }
+}
---
+++ Test3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int num1;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        num1= input.nextInt();
+        
+       if (num1 >=90){
+           System.out.println("vevr good");
+       }
+       else if (num1 >= 70) {
+           System.out.println("good");
+       }
+       else if (num1 >= 60) {
+           System.out.println("badboy");
+        }
+    }
+}
