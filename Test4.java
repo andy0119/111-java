ebff063bff43958b197b398bacd3eb9c68a1787d
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-10-07 08:33:34 +0800

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int grade;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer:");
+        grade= input.nextInt();
+       }
+       if (grade >= 60 & grade >= 70) {
+           System.out.println("<=70");
+       else {
+           System.out.println("<60");
+        }
+    }
+}
