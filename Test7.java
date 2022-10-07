b06851bc18fbf0e1f83f079420e982158865ff73
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-10-07 10:57:36 +0800

    Add files via upload

---
+++ Test7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test7 {
+    public static void main(String[] args) {
+        int grade = 9 ;
+                
+        while (grade >=0) {
+            System.out.printf("%d %n" ,grade);
+            grade = grade - 1;
+        }
+    }
+}
