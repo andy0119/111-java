b50d829c4d8cc5f505828805122d6b1901a77cce
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-10-14 11:03:55 +0800

    Add files via upload

---
+++ Test11.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import javax.swing.tree.RowMapper;
+
+public class Test11 {
+    public static void main(String[] args) {
+        int row = 0 ;
+        int t ;
+
+        while (row < 9){
+           t = row ;
+           if (row >= 5){
+                t = 8 - row ;
+            }
+            int star = 0;
+            while (star <= t) {
+                System.out.print("*") ;
+                star = star + 1;
+            }
+                System.out.println(" ") ;
+                row = row + 1;        
+        }
+    }
+}
+//* 
+//**
+//*** 
+//****
+//*****
+//****
+//***
+//**
+//* 
+//
