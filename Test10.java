6766eb90f5361b30f52aa8fef8e588ac08d0d3f7
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-10-14 10:49:50 +0800

    Add files via upload

---
+++ Test10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Test10 {
+    public static void main(String[] args) {
+        int row = 0 ;
+        int row1 = 0 ;
+
+        while (row < 5) {
+            int star = 0;
+            while (star <= row) {
+                System.out.printf("*") ;
+                star = star + 1;
+                }
+                System.out.println(" ") ;
+                row = row + 1;
+            }
+        while (row1 < 5) {
+                int star1 = 4;
+                    while (star1 > row1) {
+                        System.out.printf("*") ;
+                        star1 = star1 - 1;
+                        }
+                        System.out.println(" ") ;
+                        row1 = row1 +1;
+                 }
+             }
+        }
+
