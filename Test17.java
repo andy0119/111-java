ce44258a20eb43b33ddf6514316a8e329e8746e8
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-10-21 11:59:17 +0800

    Add files via upload

---
+++ Test17.java
@@ -0,0 +1,27 @@
+public class Test17 {
+    public static void main(String[] args) {
+    int a,i,i2,j;
+    int b = 1;
+    String str1,str2 ="";
+    
+    for (i=-5; i<1 ; i++){
+        i2 = Math.abs(i);
+        str1 = "";
+        
+        for(j=1; j<i2+1;j++){
+            str1 = str1 +"*";
+        }
+            str2 +=String.valueOf(b);
+            System.out.println(str1 + str2);
+            b +=1;
+           
+        }
+    }
+}
+// print 
+// *****1
+// ****12
+// ***123
+// **1234
+// *12345
+// 123456
