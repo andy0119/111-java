36de67bf7eb8465afddc644e9402613016febb12
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-11-04 08:48:27 +0800

    Add files via upload

---
+++ Test19.java
@@ -0,0 +1,11 @@
+public class Test19 {
+    public static void main(String[] args){
+        float a = 0.0f ;
+        float b = 0.2f ;
+        a += 0.2;
+        if (a == b)
+            System.out.printf("a == b ");
+        else 
+            System.out.printf("a != b");
+    }
+}
