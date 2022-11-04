39fc7fe4133ce05be30cda25b29f89256340405c
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-11-04 08:52:16 +0800

    Update Test19.java

--- Test19.java
+++ Test19.java
@@ -1,8 +1,15 @@
 public class Test19 {
     public static void main(String[] args){
         float a = 0.0f ;
-        float b = 0.2f ;
-        a += 0.2;
+        float b = 0.8f ;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
+        a += 0.1;
         if (a == b)
             System.out.printf("a == b ");
         else 
