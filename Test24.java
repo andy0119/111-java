6b42602170f67d50b0e9fd8c07af31d9b66d76d1
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-11-25 11:07:35 +0800

    Add files via upload

---
+++ Test24.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Test24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("enter your km:");
+
+        int a = input.nextInt();
+            System.out.println("total price:");
+            if(a > 7){
+                System.out.println(  ( 85+ (a-7) * 5 )  );
+            }
+            else
+                System.out.println("85");  
+    }
+}
+/*
+計算TEXT車資
+7公里內85元 7公里後就每公里5元
+ */
+ 
