c27241c8dae4b4978f2ec53a2cf776e600ce5443
andy0119 <113650347+andy0119@users.noreply.github.com>
2022-11-25 10:10:41 +0800

    Add files via upload

---
+++ Test23a2.java
@@ -0,0 +1,24 @@
+public class Test23a2 {
+    public static void main(String[] args) {
+        for (int i=-4 ; i <= 4 ; i++){
+            for (int k= -5 ;k <= 5 ; k++){
+                if (Math.abs(k) + Math.abs(i) > 4)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+/*
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+**       **
+***     ***
+****   ****
+***** *****
+ */
