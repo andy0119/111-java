e49e2e25a1eaa209c8df2573a46e31ab4a70726b
AndyXZP <113650347+andy0119@users.noreply.github.com>
2023-05-12 08:51:37 +0800

    Add files via upload

---
+++ Test33/Human.java
@@ -0,0 +1,31 @@
+class Human{
+    String sex;
+    int heigth ;
+    int weight ;
+
+    public Human(String sex, int heigth, int weigth){
+    this.sex = sex;
+    this.heigth = heigth;
+    this.weight = weight;
+    }
+    
+    public void printHight(){
+        System.out.println(heigth);
+    }
+    public void printWeight(){
+        System.out.println(weight);
+    }
+}
+/*
+public class Test33{
+    public static void main(String[]args){
+        Human human1 = new Human("man", 170, 75);
+        Human human2 = new Human("man", 160, 70);
+        human1.printHight();
+        //human1.printWeight();
+        human2.printHight();
+        //human2.printWeight();
+       
+    }
+}
+*/
